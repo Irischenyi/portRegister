985ba4877c6fd564fd73bb9296681ee5c6395394
sly0809 <154038185@qq.com>
2024-05-10 19:13:30 +0800

    app.vue页面优化

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import InputTemplate from './components/InputTemplate.vue'
+import MainBody from '@/views/main/MainBody.vue'
 </script>
 
 <template>
   <div>
-    <div class="footer_wrap">
-      <div class="title">数据出境公共服务平台</div>
-      <div style="margin-right: 600px">
-        <RouterLink class="routey" to="/home">首页</RouterLink>
-        <RouterLink class="routey" to="/serviceHall">服务大厅</RouterLink>
-        <RouterLink class="routey" to="/complianCetopics">合规专题</RouterLink>
-        <RouterLink class="routey" to="/policyconSultation"
-          >政策咨讯</RouterLink
-        >
-        <RouterLink class="routey" to="/securityTraining">安全培训</RouterLink>
-      </div>
-      <div>
-        <q-btn color="white" text-color="black" label="调研填表" />
-        <q-btn color="primary" label="登陆注册" />
-        <!--  -->
-      </div>
-    </div>
-    <div class="public">
-      <RouterView />
-    </div>
+    <MainBody></MainBody>
   </div>
-
-  <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-.footer_wrap {
-  /* position: fixed;
-  left: 0;
-  top: 0; */
-  display: flex;
-  padding: 20px 50px;
-  background-color: #fff;
-  width: 100%;
-  margin: 0 auto;
-}
-/* .public {
-  width: 1500px;
-  margin: 0 auto;
-  background-color: red;
-} */
-.routey {
-  color: #000;
-  text-decoration: none;
-  margin-right: 50px;
-}
-.title {
-  font-size: 18px;
-  margin-right: 150px;
-}
-</style>
+<style scoped></style>
