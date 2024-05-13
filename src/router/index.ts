f54cecf577ab484054d5ba40b1a2e2ef5e4b43f0
irisChenyi <chenyi4as@163.com>
2024-05-13 20:21:19 +0800

    add router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,10 +5,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: '/login',
-    },
-    {
       path: '/index',
       name: 'index',
       redirect: '/index/home',
