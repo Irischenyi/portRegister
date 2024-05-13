c8347755a879b2669dd96f2dab43536c77bc8505
irisChenyi <chenyi4as@163.com>
2024-05-13 18:05:03 +0800

    change config

---
+++ dist/assets/zczx-D65g3fBc.js
@@ -0,0 +1,1 @@
+const r=""+new URL("zczx-B0gi7T8_.png",import.meta.url).href;export{r as _};
--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       }
     }
   },
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
