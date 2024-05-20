6a4468fabfee5f69527afc19b2ad29388f95681e
irisChenyi <chenyi4as@163.com>
2024-05-20 18:44:07 +0800

    add personal page

---
+++ src/views/personal/Main.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="header"></div>
+        <div class="body">
+            <div class="left">
+              <q-list bordered separator>
+                <q-item clickable v-ripple>
+                  <q-item-section>基础信息</q-item-section>
+                </q-item>
+                <q-item clickable v-ripple>
+                  <q-item-section>消息中心</q-item-section>
+                </q-item>
+                <q-item clickable v-ripple>
+                  <q-item-section>我的流程</q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+            <div class="right">
+              <RouterView />
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.header{
+  width: 100%;
+  height: 3vw;
+  background-color: #4984FF;
+}
+.body{
+  width: 100%;
+  height: calc(100vh - 3vw);
+  background: #e9eff9;
+  .left{
+    width: 250px;
+    height: 100%;
+    background-color: #eef4ff;
+    display: inline-block;
+    vertical-align: top;
+  }
+  .right{
+    width: calc(100% - 280px);
+    height: calc(100% - 20px);
+    margin-left: 15px;
+    margin-top: 10px;
+    border-radius: 10px;
+    background: white;
+    display: inline-block;
+    vertical-align: top;
+    box-shadow: 0 0 10px rgba(0,0,0,0.06);
+  }
+}
+
+.q-list--separator > .q-item-type + .q-item-type{
+  border: none;
+}
+.q-list--bordered{
+  border: none;
+  font-size: 14px;
+  color: #787878;
+}
+</style>
+  
---
+++ src/views/personal/MessageCenter.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="center-box">
+      <div class="text-h6">
+        消息中心
+      </div>
+      <div class="tabs">
+        <q-tabs
+            v-model="tab"
+            dense
+            class="text-grey"
+            active-color="primary"
+            indicator-color="primary"
+            align="justify"
+            narrow-indicator
+          >
+            <q-tab name="mails" label="全部(10)" />
+            <q-tab name="alarms" label="未读(06)" />
+            <q-tab name="movies" label="已读(04)" />
+          </q-tabs>
+          <q-separator />
+      </div>
+      
+      <q-select
+        filled
+        v-model="model"
+        :options="options"
+        style="width: 250px"
+      />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="mails">
+          <el-table
+            ref="multipleTable"
+            :data="tableData"
+            tooltip-effect="dark"
+            style="width: 100%"
+            @selection-change="handleSelectionChange"
+          >
+            <el-table-column type="selection" width="55"> </el-table-column>
+            <el-table-column label="日期" width="120">
+              <template #default="scope">{{ scope.row.date }}</template>
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+            <el-table-column prop="address" label="地址" show-overflow-tooltip>
+            </el-table-column>
+          </el-table>
+        </q-tab-panel>
+
+        <q-tab-panel name="alarms">
+          <div class="text-h6">Alarms</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="movies">
+          <div class="text-h6">Movies</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
+      
+      <div>
+        <el-pagination layout="prev, pager, next" :total="20"> </el-pagination>
+      </div>
+      <div class="footer">
+        <q-btn unelevated color="grey-5" label="返回" />
+        <q-btn unelevated color="primary" label="提交更新" />
+      </div>
+    </div>
+  </template>
+  <script setup lang="ts">
+  import { ref, watch } from 'vue';
+  const tableData = ref([{
+      date: '2016-05-03',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+    },
+    {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+    },
+    {
+      date: '2016-05-04',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+    },
+    {
+      date: '2016-05-01',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+    },
+    {
+      date: '2016-05-01',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+    },
+    {
+      date: '2016-05-01',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+    }])
+  const handleSelectionChange = () =>{}
+  const tab = ref('mails')
+  const model = ref('')
+  const options = ref(['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'])
+
+  </script>
+  <style lang="scss" scoped>
+  .el-pagination{
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .tabs{
+    width: 50%;
+  }
+
+  .center-box{
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    padding: 30px;
+    position: relative;
+  }
+
+  .text-h6{
+    margin-bottom: 20px;
+  }
+
+  .q-select{
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  .footer{
+    display: flex;
+    justify-content: end;
+    position: absolute;
+    bottom: 15px;
+    right: 15px;
+    button{
+      margin-left: 10px;
+    }
+  }
+  </style>
+  
